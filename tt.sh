467b177e58b2a2130f489963e7c8151601f9d2ca
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2022-06-28 17:47:08 +0530

    Update tt.sh

--- tt.sh
+++ tt.sh
@@ -2,6 +2,8 @@
 echo "this is shell second scirpt"
 name=$1
 echo "your name is $name"
-touch /home/ubuntu/new/$name/new/t.txt
-touch /home/ubuntu/new/asdf/new/t.txt
+echo "/home/ubuntu/new/$name/new/t.txt"
+whoami
+#touch /home/ubuntu/new/$name/new/t.txt
+#touch /home/ubuntu/new/asdf/new/t.txt
 
